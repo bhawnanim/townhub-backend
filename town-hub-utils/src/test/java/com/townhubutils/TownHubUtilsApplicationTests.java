f4bb35de88f431175f80d040439bc928758d81bd
priyanka <priyanka@bbd.co.za>
2020-05-07 16:20:23 +0530

    changes

---
+++ town-hub-utils/src/test/java/com/townhubutils/TownHubUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.townhubutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TownHubUtilsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
