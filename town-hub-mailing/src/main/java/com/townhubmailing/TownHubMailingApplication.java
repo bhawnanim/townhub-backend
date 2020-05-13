6fc96e9ecd4c5c52cafee9e8e52f55e6208e87be
Mukesh Bhawnani <mukesh@bbd.co.za>
2020-05-14 04:40:49 +0530

    Mail Service Added Mail Service Configuration is added Mail Service added in zuul config

--- town-hub-mailing/src/main/java/com/townhubmailing/TownHubMailingApplication.java
+++ town-hub-mailing/src/main/java/com/townhubmailing/TownHubMailingApplication.java
@@ -1,9 +1,13 @@
 package com.townhubmailing;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+@EnableEncryptableProperties
 public class TownHubMailingApplication {
 
     public static void main(String[] args) {
